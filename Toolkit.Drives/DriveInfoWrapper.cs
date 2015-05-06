6e061208e3c3cd3b8e50d284240d4da3abf6e072
Uri <sakura256@gmail.com>
2015-05-06 17:40:19 +0200

    Initial commit for bullshit stuff.

---
+++ Toolkit.Drives/DriveInfoWrapper.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Toolkit.Drives
+{
+    public class DriveInfoWrapper
+    {
+        public enum UnitSize
+        {
+            KiloByte,
+            MegaByte,
+            GigaByte,
+            TeraByte
+        }
+
+
+        private DriveInfo driveInfo;
+
+        public DriveInfoWrapper(DriveInfo di)
+        {
+            driveInfo = di;
+        }
+
+        public DriveType DriveType
+        {
+            get
+            {
+                return (driveInfo.DriveType);
+            }
+        }
+
+        public int GetTotalSize(UnitSize unitSize = UnitSize.GigaByte)
+        {
+            int ret = 0;
+
+            ret = GetSizeInUnit(driveInfo.TotalSize, unitSize);
+
+            return (ret);
+        }
+
+        public int GetTotalFreeSpace(UnitSize unitSize = UnitSize.GigaByte)
+        {
+            int ret = 0;
+
+            ret = GetSizeInUnit(driveInfo.TotalFreeSpace, unitSize);
+
+            return (ret);
+        }
+
+        private int GetSizeInUnit(long size, UnitSize unitSize = UnitSize.GigaByte)
+        {
+            int ret = 0;
+            switch(unitSize)
+            {
+                case UnitSize.TeraByte:
+                    ret = GetSizeInUnit(size, UnitSize.GigaByte) / 1024;
+                    break;
+                case UnitSize.GigaByte:
+                    ret = GetSizeInUnit(size, UnitSize.MegaByte) / 1024;
+                    break;
+                case UnitSize.MegaByte:
+                    ret = GetSizeInUnit(size, UnitSize.KiloByte) / 1024;
+                    break;
+                case UnitSize.KiloByte:
+                    ret = (int)(size / 1024);
+                    break;
+            }
+            return (ret);
+        }
+
+
+    }
+}
---
+++ Toolkit.Windows/Updates.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management;
+using System.Text;
+using WUApiLib;
+
+namespace Toolkit.Windows
+{
+    public class Updates
+    {
+
+        public static bool GetKBInstalledByWMI(string KbId)
+        {
+            bool ret = false;
+
+            const string query = "SELECT HotFixID FROM Win32_QuickFixEngineering";
+            var search = new ManagementObjectSearcher(query);
+            var collection = search.Get();
+
+            List<string> updates = new List<string>();
+
+            foreach (ManagementObject quickFix in collection)
+                updates.Add(quickFix["HotFixID"].ToString());
+            var singleUpdate = updates.Where(x => x.Contains(KbId));
+            
+            if(singleUpdate.Count() > 0)
+            {
+                ret = true;
+            }
+
+            return (ret);
+        }
+
+        public static bool GetKBInstalledByWUApi(string KbId)
+        {
+            bool ret = false;
+
+            var updateSession = new UpdateSession();
+            var updateSearcher = updateSession.CreateUpdateSearcher();
+            updateSearcher.IncludePotentiallySupersededUpdates = true;
+            var count = updateSearcher.GetTotalHistoryCount();
+            if (count > 0)
+            {
+                var history = updateSearcher.QueryHistory(0, count);
+
+                List<string> updates = new List<string>();
+
+                for (int i = 0; i < count; ++i)
+                    updates.Add(history[i].Title);
+
+                var singleUpdate = updates.Where(x => x.Contains(KbId));
+                if(singleUpdate.Count() > 0)
+                {
+                    ret = true;
+                }
+            }
+            return (ret);
+        }
+    }
+}
